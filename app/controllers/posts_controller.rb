f0e92c0e366f1a984cbee3f59a4db19fb32f01c9
Adam Duff <j.adam.duff@gmail.com>
2018-09-27 23:48:16 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,17 @@ class PostsController < ApplicationController
   helper_method :params
 
   def index
+
+    if !params[:author].blank?
+      @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      end
+    end
+    
     @posts = Post.all
   end
 
