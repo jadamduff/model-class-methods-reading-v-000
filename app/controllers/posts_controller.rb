7e18c46e28dd655461e7b74a3efa9f94c8ac57a5
Adam Duff <j.adam.duff@gmail.com>
2018-09-27 23:48:27 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
 
   def index
 
+    @authors = Author.all
+
     if !params[:author].blank?
       @posts = Post.where(author: params[:author])
     elsif !params[:date].blank?
