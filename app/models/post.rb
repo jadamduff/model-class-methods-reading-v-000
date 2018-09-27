254050f182073d520c2970bf09b736e3761edb2c
Adam Duff <j.adam.duff@gmail.com>
2018-09-27 23:54:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,6 +18,9 @@ class Post < ActiveRecord::Base
     self.title = self.title.titlecase
   end
 
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
 
   def self.from_today
     where("created_at >=?", Time.zone.today.beginning_of_day)
